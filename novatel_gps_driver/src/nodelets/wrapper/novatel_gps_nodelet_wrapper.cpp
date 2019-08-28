358b0624136903e9d0d64a6e7cd5520a0a28b3ae
Michael McConnell <Michael.McConnell-2@leidos.com>
2019-08-28 12:51:23 -0400

    Correct name access in nodelet wrapper

--- novatel_gps_driver/src/nodelets/wrapper/novatel_gps_nodelet_wrapper.cpp
+++ novatel_gps_driver/src/nodelets/wrapper/novatel_gps_nodelet_wrapper.cpp
@@ -89,7 +89,7 @@ namespace novatel_gps_driver
         ros::CARMANodeHandle pnh(getPrivateNodeHandle());
 
         publisher_.reset(new APIPublisher());
-        publisher_->setName(ros::this_node::getName());
+        publisher_->setName(getName());
 
         publisher_->fix_fused_pub_ = nh.advertise<gps_common::GPSFix>("gnss_fix_fused", 10);
         publisher_->status_pub_ = nh.advertise<cav_msgs::DriverStatus>("driver_discovery", 10);
